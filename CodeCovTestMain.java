9e6ea5d2a42306998fb8cfc4807dd2b9c4cd5731
janosmurai <murai.janos@gmail.com>
2015-11-16 22:28:02 +0100

    inital commit

---
+++ CodeCovTestMain.java
@@ -0,0 +1,39 @@
+package code_coverage_test_java;
+
+public class CodeCovTestMain {
+	
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int i = 0;
+        int mult = 0;
+        
+        if (a > b)
+        {
+        	while(a > b)
+        	{
+        		b ++;
+        	}
+        	System.out.println("a is greater then b");
+        }
+        else if (a == b)
+        {
+        	System.out.println("a and b are equal");
+        }
+        else
+        {
+        	System.out.println("b is greater then a");
+        }
+        
+        for (i = 0; i < a; i++)
+        {
+        	if((b != 0) && (a < 200))
+        	{
+        		mult = i * b;
+        		System.out.println(mult);
+        	}
+        }
+        
+    }
+
+}
